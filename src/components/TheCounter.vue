63cff32a4bf12e3f3cb9bd9426ea75861221cd21
mygit <purceladzedavit@gmail.com>
2023-02-19 14:18:50 +0400

    add mapGetters wich will dig inside of state instead of us

--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -1,13 +1,18 @@
 <template>
-  <h3>{{ counter }}</h3>
+  <h3>{{ finalCounter }}</h3>
 </template>
 
 <script>
+//we are importing mapGetters from 'vuex'
+//it will dig in state instead of us
+import { mapGetters } from 'vuex';
 export default {
   computed: {
-    counter() {
-      return this.$store.getters.finalCounter;
-    },
+    // counter() {
+    //   return this.$store.getters.finalCounter;
+    // },
+
+    ...mapGetters(['finalCounter']), // ...mapGetters([]) in the aray we can use any name of getter in string and use it in this component
   },
 };
 </script>
